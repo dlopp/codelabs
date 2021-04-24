9368d11b2122b0d7c8d71996c0672ab081bc8e37
Daaiki <daaikii@outlook.jp>
2021-04-24 18:57:15 +0900

    create nuxt app

---
+++ sources/todo-app-for-web/client/nuxt.config.ts
@@ -0,0 +1,39 @@
+import { NuxtConfig } from '@nuxt/types'
+
+const config: NuxtConfig = {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'client',
+    htmlAttrs: {
+      lang: 'ja'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Todoアプリを作るハンズオン用のサンプルコード' }
+    ],
+  },
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+}
+
+export default config
