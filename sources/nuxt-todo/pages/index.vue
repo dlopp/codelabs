aa9bb23a56ba2ab207370778accff2b72ee51bf1
Daaiki <daaikii@outlook.jp>
2021-05-23 23:28:39 +0900

    htmlを書く

--- sources/nuxt-todo/pages/index.vue
+++ sources/nuxt-todo/pages/index.vue
@@ -1,5 +1,31 @@
 <template>
-   
+  <div class="wrapper">
+    <header class="header">
+      <div class="container">
+        <h1 class="headline">Todo App for Web</h1>
+      </div>
+    </header>
+    <section class="main">
+      <ul>
+        <li class="card">
+          <p class="todo">todo1です</p>
+          <button class="done">
+            完了
+          </button>
+        </li>
+        <li class="card">
+          <p class="todo">todo2です</p>
+          <button class="done">
+            完了
+          </button>
+        </li>
+      </ul>
+      <form class="form">
+        <input type="text" class="input" />
+        <button type="submit" class="button">Todo 登録</button>
+      </form>
+    </section>
+  </div>
 </template>
 
 <script>
