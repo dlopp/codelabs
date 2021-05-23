6264ac4e4951190682e79705d18ba94fd70f84a6
Daaiki <daaikii@outlook.jp>
2021-05-23 23:36:37 +0900

    CSSを書く

--- sources/nuxt-todo/pages/index.vue
+++ sources/nuxt-todo/pages/index.vue
@@ -35,5 +35,102 @@ export default {
 </script>
 
 <style>
+:root {
+  --main-color: #93c5fd;
+  --hover-color: #23c5fd;
+}
 
+* {
+  margin: 0;
+  box-sizing: border-box;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+/* Todo App の Style */
+.wrapper {
+  min-height: 100vh;
+  position: relative;
+}
+
+.main {
+  margin: 10px auto;
+  max-width: 36rem;
+}
+
+.container {
+  max-width: 36rem;
+  margin: 0 auto;
+}
+
+/* headerのstyle */
+.header {
+  height: 96px;
+  width: 100%;
+  background-color: var(--main-color);
+}
+
+.headline {
+  font-size: 30px;
+  line-height: 96px;
+  margin-left: 20px;
+  font-weight: 700;
+  color: white;
+}
+
+/* todoリストのstyle */
+.card {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+/* 入力フォームのstyle */
+.form {
+  display: flex;
+  padding: 0 20px;
+  position: absolute;
+  bottom: 20px;
+}
+
+.input {
+  border-radius: 5px;
+  font-size: 20px;
+  padding-left: 10px;
+  width: 400px;
+  margin-right: 10px;
+}
+
+.button {
+  border-radius: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+  font-size: 20px;
+  font-weight: bold;
+  background: var(--main-color);
+  color: white;
+  border: none;
+}
+
+.button:hover {
+  background-color: var(--hover-color);
+}
+
+.todo {
+  font-size: 1.5rem;
+  margin-top: 5px;
+}
+
+.done {
+  padding: 5px 10px;
+  font-weight: bold;
+  border-radius: 4px;
+  color: white;
+  background-color: var(--main-color);
+  border: none;
+  cursor: pointer;
+}
+
+.done:hover {
+  background-color: var(--hover-color);
+}
 </style>
